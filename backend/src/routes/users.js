84c7b9bfbf6abcaa0064657fe849a3ed07f507e9
Luiz-Pastor <luigpacr5@gmail.com>
2025-02-21 23:23:32 +0100

    Create a simple API to take reference on the database querys and use of the request/reply

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -1,5 +1,30 @@
-function get_users(request, reply) {
-	return fastify.db.all('SELECT * FROM users');
+import {db} from '../database/database.js'
+
+export function get_users(request, reply) {
+	try {
+		const users = db.prepare('SELECT * FROM users').all();
+		reply.send(users);
+	}
+	catch( error )
+	{
+		reply.status(500).send("Error while checking the database: ", error);
+	}
 }
 
-export default get_users;
+export function add_user(request, reply) {
+	try {
+		const { name, email } = request.body;
+		const data = db.prepare("SELECT * FROM users WHERE name = ? OR email = ?").get(name, email);
+		if (data === undefined) {
+			db.prepare("INSERT INTO users (name, email) VALUES (?, ?)").run(name, email)
+			reply.send("User added correctly!")
+		} else  {
+			reply.send("Error: an user with this data has been registered before")
+		}
+
+	}
+	catch( error )
+	{
+		reply.status(500).send(error)
+	}
+}
