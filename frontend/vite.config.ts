5688ea684c13c819712a9c3c0e74d517531ada92
Luiz-Pastor <luigpacr5@gmail.com>
2025-02-24 10:15:24 +0100

    Changed the config so vite can listen the changes produced

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true, // Permite que Vite detecte cambios en Docker
+    },
+    host: true, // Hace que Vite escuche en 0.0.0.0
+    port: 5173, // Asegura que usa el puerto correcto
+  },
 })
