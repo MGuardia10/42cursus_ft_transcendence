f54a6fa68d1a592cadc41ab2669d2a7db87d1dad
Luiz-Pastor <luigpacr5@gmail.com>
2025-03-05 22:55:49 +0100

    Deleted the languages table, and added on cascade restrictions on the friends table, so when a user is deleted any problem occurs

--- backend-user/src/database/schema.sql
+++ backend-user/src/database/schema.sql
@@ -1,23 +1,9 @@
-
--- Languages
-CREATE TABLE IF NOT EXISTS languages (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name TEXT UNIQUE
-);
-
--- Users:
---   · id
---   · Nombre de perfil (alias)
---   · Email
---   · Avatar (url)
---   · Lenguaje (es/en...)
+-- Users
 CREATE TABLE IF NOT EXISTS users (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name TEXT NOT NULL,
+  name TEXT NOT NULL UNIQUE,
   email TEXT NOT NULL UNIQUE,
-  language INTEGER NOT NULL,
-
-  FOREIGN KEY (language) REFERENCES languages(id)
+  avatar TEXT
 );
 
 
@@ -28,16 +14,12 @@ CREATE TABLE IF NOT EXISTS friend_status (
 );
 
 -- Amigos:
---   · Id 1
---   · Id 2
---   · Status
-
 CREATE TABLE IF NOT EXISTS friends (
   user_a INTEGER NOT NULL,
   user_b INTEGER NOT NULL,
-  status INTENER NOT NULL,
+  status INTEGER NOT NULL,
 
-  FOREIGN KEY user_a REFERENCES users(id),
-  FOREIGN KEY user_b REFERENCES users(id),
-  FOREIGN KEY status REFERENCES friend_status(id)
+  FOREIGN KEY (user_a) REFERENCES users(id) ON DELETE CASCADE,
+  FOREIGN KEY (user_b) REFERENCES users(id) ON DELETE CASCADE,
+  FOREIGN KEY (status) REFERENCES friend_status(id)
 );
