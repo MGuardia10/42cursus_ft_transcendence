e95b6a6c62f351d3282f2f507a1aeacb8db539d8
Luiz-Pastor <luigpacr5@gmail.com>
2025-03-07 18:48:05 +0100

    Updated the route and implemented the endpoint

--- backend-user/src/routes/add_user.js
+++ backend-user/src/routes/add_user.js
@@ -1,3 +1,51 @@
-export default async function add_user( request, reply ) {
-	
+import path from 'path';
+import db from '../database/database.js'
+import { randomBytes } from 'crypto';
+import 'dotenv/config'
+import fs from 'fs';
+
+export default async function add_user(request, reply) {
+
+	/* Get the json values */
+	const { name, email, avatar_url } = request.body;
+
+	/* Check if any user with that email or username exists */
+	const username_search = db.prepare("SELECT * FROM users WHERE name = ?").get(name);
+	if (username_search)
+		return reply.code(409).send({ error: "The name is already used by another user" })
+
+	const email_search = db.prepare("SELECT * FROM users WHERE email = ?").get(email);
+	if (email_search)
+		return reply.code(409).send({ error: "The email is already used by another user" })
+
+	/* Get the image, generate a name and save it on a file */
+	const image_name = randomBytes(16).toString('hex');
+	const image_path = path.join(process.env.AVATAR_FOLDER, `${image_name}.jpg`)
+
+	try
+	{
+		const response = await fetch(avatar_url);
+		if (!response.ok)
+			throw new Error(response.statusText);
+
+		const buffer = await response.arrayBuffer();
+		await fs.promises.writeFile(image_path, Buffer.from(buffer));
+	}
+	catch(err)
+	{
+		return reply.code(404).send({ error: `Failed to get the user avatar from the url: ${err.message}` });
+	}
+
+	/* Insert the data */
+	let user_id;
+	try
+	{
+		const queryStatus = db.prepare("INSERT INTO users (name, email, avatar) VALUES (?, ?, ?)").run(name, email, image_path);
+		user_id = queryStatus.lastInsertRowid;
+	}
+	catch(err)
+	{
+		return reply.code(500).send({ error: 'Erro while saving the info on the database' });
+	}
+	return reply.code(201).send({ id: user_id, name: name, email: email })
 }
--- backend-user/src/routes/routes.js
+++ backend-user/src/routes/routes.js
@@ -1,15 +1,17 @@
-import help from './help.js'
-import {add_user, get_users} from './users.js'
+import add_user from './add_user.js'
 
 export default async function (fastify, options) {
-  fastify.post('/users', {
+  
+  /* Endpoint to add users */
+  fastify.post('/', {
     schema: {
       body: {
         type: 'object',
         required: ['name', 'email'],
         properties: {
           name: { type: 'string' },
-          email: { type: 'string', format: 'email' }
+          email: { type: 'string', format: 'email' },
+          avatar_url: { type: 'string', format: 'uri' }
         }
       }
     }
