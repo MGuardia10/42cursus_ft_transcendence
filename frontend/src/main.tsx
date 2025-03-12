bb9970b5934a10712c1185801bea216b960e1ba1
Miguel Guardia <mike.guardia0309@gmail.com>
2025-03-12 17:56:14 +0100

    defined all routes && navbar layout done

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import RouterApp from './router/RouterApp.tsx'
+import RouterApp from '@/router/RouterApp.tsx'
+import { LanguageProvider } from '@/context/Language/LanguageProvider.tsx'
+import { NotificationProvider } from '@/context/Notification/NotificationProvider.tsx'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-      <RouterApp />
+    <LanguageProvider>
+      <NotificationProvider>
+        <main className='max-h-screen width-full'>
+          <RouterApp />
+        </main>
+      </NotificationProvider>
+    </LanguageProvider>
   </StrictMode>,
 )
--- frontend/src/router/RouterApp.tsx
+++ frontend/src/router/RouterApp.tsx
@@ -8,6 +8,8 @@ import LeaderBoard from '@/pages/Leaderboard/Leaderboard';
 import Login from '@/pages/Login/Login';
 import Profile from '@/pages/Profile/Profile';
 import Settings from '@/pages/Settings/Settings';
+import SingleMatch from '@/pages/SingleMatch/SingleMatch';
+import Tournament from '@/pages/Tournament/Tournament';
 
 
 const RouterApp: React.FC = () => {
@@ -20,10 +22,12 @@ const RouterApp: React.FC = () => {
 
 				<Route element={<AppLayout />}>
 					<Route path="/" element={<Home />} />
+					<Route path="/single-match" element={<SingleMatch />} />
+					<Route path="/tournament" element={<Tournament />} />
 					<Route path="/dashboard" element={<Dashboard />} />
 					<Route path="/leaderboard" element={<LeaderBoard />} />
 					<Route path="/profile" element={<Profile />} />
-					<Route path="/settings" element={<Settings />} />
+					<Route path="/game-settings" element={<Settings />} />
 				</Route>
 
 				<Route path="*" element={<h1>Not Found</h1>} />
