afc6f20d114d93cdb81858499a583ed10c1d7dd5
Luiz-Pastor <luigpacr5@gmail.com>
2025-03-14 23:11:39 +0100

    Commented the function and added a field on the where items

--- backend-user/src/database/database_query.js
+++ backend-user/src/database/database_query.js
@@ -5,7 +5,12 @@ class DatabaseQuery
 	#where;
 	#limit;
 	#offset;
-	#params;
+	/**
+	 * Constructs a new instance of a database query object.
+	 *
+	 * @param {string} table - The name of the database table to query.
+	 * @param {string[]} [fields=["*"]] - The fields to select in the query. Defaults to all fields ("*").
+	 */
 	constructor(table, fields)
 	{
 		this.#select = fields || ["*"];
@@ -21,6 +26,7 @@ class DatabaseQuery
 	 * @param {Array<{key: string, value: *}>} items - An array of objects representing the conditions, 
 	 * where each object contains:
 	 *   - `key` (string): The column name to apply the condition to.
+	 *   - `action` (string): The action to perform on the key-value ('=', '>', 'LIKE', ...).
 	 *   - `value` (*): The value to compare the column against.
 	 * 
 	 * @param {string} operator - The logical operator to use in the WHERE condition ('=', 'AND', 'OR', ...).
@@ -34,16 +40,46 @@ class DatabaseQuery
 		});
 	}
 
+	/**
+	 * Sets the LIMIT clause for the query.
+	 *
+	 * @param {number} limit - The maximum number of rows to return. Defaults to -1 (no limit).
+	 */
 	add_limit(limit = -1)
 	{
 		this.#limit = limit;
 	}
 
+	/**
+	 * Sets the OFFSET clause for the query.
+	 *
+	 * @param {number} offset - The number of rows to skip before starting to return rows. Defaults to -1 (no offset).
+	 */
 	add_offset(offset = -1)
 	{
 		this.#offset = offset;
 	}
 
+	/**
+	 * Generates an SQL query string based on the current state of the object.
+	 * The query includes SELECT, FROM, WHERE, LIMIT, and OFFSET clauses, 
+	 * and supports parameterized queries to prevent SQL injection.
+	 *
+	 * @returns {Object} An object containing:
+	 *   - `query` {string}: The generated SQL query string.
+	 *   - `params` {Array}: An array of parameters to be used in the query.
+	 *
+	 * @example
+	 * // Example usage:
+	 * const result = generate();
+	 * console.log(result.query); // "SELECT column1, column2 FROM table WHERE (key1 = ?) OR (key2 = ?) LIMIT ? OFFSET ?"
+	 * console.log(result.params); // ["value1", "value2", 10, 5]
+	 *
+	 * @description
+	 * - The `SELECT` and `FROM` clause are set on constructor
+	 * - The `WHERE` clause is constructed from the `add_where` method.
+	 * - The `LIMIT` and `OFFSET` clauses are added on `add_limit` and `add_offset` methods.
+	 */
 	generate()
 	{
 		/* Select */
@@ -69,7 +105,7 @@ class DatabaseQuery
 				let condition = "";
 				for (let j = 0; j < this.#where[i].items.length; j++)
 				{
-					condition += `${this.#where[i].items[j].key} = ?`;
+					condition += `${this.#where[i].items[j].key} ${this.#where[i].items[j].action} ?`;
 					params.push(this.#where[i].items[j].value);
 					if (j < this.#where[i].items.length - 1)
 						condition += ` ${this.#where[i].operator} `;
