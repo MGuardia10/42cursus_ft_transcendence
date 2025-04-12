7cff4c2c1ce80d11077f7300b546616442a38d0b
Luiz-Pastor <luigpacr5@gmail.com>
2025-04-12 18:25:24 +0200

    Implemented the creation of new users

--- backend-auth/src/routes/google_callback.js
+++ backend-auth/src/routes/google_callback.js
@@ -1,26 +1,67 @@
 import url from 'url';
 import { oauth2client } from '../google_oauth.js';
 
+async function create_user(baseurl, name, email, avatar_url)
+{
+	await fetch (`${baseurl}/`, {
+		method: "POST",
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({
+			name,
+			email,
+			avatar_url
+		})
+	})
+}
+
 export default async function google_callback(request, reply)
 {
 	/* Parse the link, divide it into parts */
-	console.log("Request: ", request.url)
 	let q = url.parse(request.url, true).query;
 	if (q.error)
 		return reply.code(500).send(q.error);
 
 	/* Get the tokens and send a request to get the info */
-	let { tokens } = oauth2client.getToken(q.code);
+	let { tokens } = await oauth2client.getToken(q.code);
 	oauth2client.setCredentials(tokens);
-	await fetch (`https://www.googleapis.com/oauth2/v3/userinfo`, {
-		headers: {
-			'Authorization': `Bearer ${tokens.access_token}`,
-		}
-	})
-		.then(response => response.json())
-		.then(data => {
-			console.log("Token data: ", data)
+
+	let google_response;
+	try {
+		google_response = await fetch (`https://www.googleapis.com/oauth2/v3/userinfo`, {
+			headers: {
+				'Authorization': `Bearer ${tokens.access_token}`,
+			}
 		})
+	}
+	catch(e)
+	{
+		return reply.code(500).send(`Error while fetching to google: ${e}`)
+	}
+
+	/* Check if the user is registered */
+	const { name, email, picture } = await google_response.json();
+	const users_api = process.env.USER_API_BASEURL_INTERNAL;
+
+	try {
+		/* Get the list */
+		const users_response = await fetch (
+			`${users_api}/?email=${email}`
+		)
+		const users_found = await users_response.json()
+		console.log("Users found: ", users_found)
+
+		/* Take action taking acoount if the user exists */
+		if (users_found.length == 0)
+			create_user(users_api, name, email, picture)
+	}
+	catch (e)
+	{
+		return reply.code(500).send(`Error fetching the user API: ${e}`)
+	}
+
+
 
 	return reply.redirect('https://localhost:8080');
 }
