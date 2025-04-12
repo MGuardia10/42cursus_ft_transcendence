ccc0678bd8fa5994ebba339d06232c4c179c4222
Luiz-Pastor <luigpacr5@gmail.com>
2025-04-12 12:16:22 +0200

    Implemented the code to make a request to get the data

--- backend-auth/src/routes/google_callback.js
+++ backend-auth/src/routes/google_callback.js
@@ -1,4 +1,37 @@
 export default async function google_callback(request, reply)
 {
+	try
+	{
+		/* Get the google validated token */
+		const token = await request.server.googleOAuth2.getAccessTokenFromAuthorizationCodeFlow(request)
+
+		/* Make the query to Google to get the data */
+		const googleResponse = await fetch(
+			'https://openidconnect.googleapis.com/v1/userinfo',
+			{
+			  headers: {
+				Authorization: `Bearer ${token.access_token}`,
+				Accept: 'application/json'
+			  }
+			}
+		  );
+		const tokenInfo = await googleResponse.json()
+		console.log("Token data: ", tokenInfo)
+
+		if (!googleResponse.ok)
+			return reply.send(await googleResponse.text())
+		
+		/* Get the fields */
+		const userData = await googleResponse.json()
+		const { name, email, picture } = userData;
+
+		console.log(`Data:\n\t· Name: ${name}\n\t· Email: ${email}\n\t· Picture: ${picture}`)
+	}
+	catch(err)
+	{
+		return reply.code(500).send(err);
+	}
+
+
 	return reply.redirect('https://localhost');
 }
