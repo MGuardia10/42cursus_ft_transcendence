62d15656df74d52d234f5200d6325c4f6025f3da
Luiz-Pastor <luigpacr5@gmail.com>
2025-04-12 15:52:07 +0200

    Deleted the use of the fastify module

--- backend-auth/src/app.js
+++ backend-auth/src/app.js
@@ -1,39 +1,18 @@
 /* fastify */
 import Fastify from 'fastify';
 import cors from '@fastify/cors';
-import fastifyOauth2 from '@fastify/oauth2';
 
 /* Local files */
 import routes from './routes/routes.js';
 
 /* NOTE: Create the server object */
-const app = Fastify({ logger: true });
+const app = Fastify({ logger: false });
 
 /* NOTE: Register the valid methods and IPs */
 app.register(cors, {
   origin: '*',
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
 });
-
-/* NOTE: Register the OAuth2 config */
-app.register(fastifyOauth2, {
-  name: 'googleOAuth2',
-  scope: [
-    'openid',
-    'https://www.googleapis.com/auth/userinfo.profile',
-    'https://www.googleapis.com/auth/userinfo.email'
-  ],
-  credentials: {
-    client: {
-      id: process.env.GOOGLE_CLIENT_ID,
-      secret: process.env.GOOGLE_CLIENT_SECRET
-    },
-    auth: fastifyOauth2.GOOGLE_CONFIGURATION,
-  },
-  startRedirectPath: '/login',
-  callbackUri: process.env.GOOGLE_CALLBACK_URI,
-});
-
 
 /* NOTE: Register the routes */
 app.register(routes);
@@ -49,4 +28,4 @@ const start = async () => {
     process.exit(1);
   }
 };
-start();  
+start();
