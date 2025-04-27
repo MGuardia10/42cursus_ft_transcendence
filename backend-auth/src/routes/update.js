0aea737aac3a000d84963324c9c2f342be1b3fc4
Luiz-Pastor <luigpacr5@gmail.com>
2025-04-27 17:46:01 +0200

    Now the language update also notifies the user api, so it can be update on the global users database

--- backend-auth/src/routes/update.js
+++ backend-auth/src/routes/update.js
@@ -1,4 +1,4 @@
-import { modify_jwt } from '../utils/jwt.js';
+import { get_jwt, modify_jwt } from '../utils/jwt.js';
 
 export default async function update (request, reply)
 {
@@ -10,13 +10,29 @@ export default async function update (request, reply)
 	const new_jwt = modify_jwt(token, 'language', language);
 
 	/* Check if the modification was executed correctly */
-	return new_jwt.valid
-		? reply.setCookie('token', new_jwt.token, {
+	if (!new_jwt.valid)
+		return reply.code(401).send({ error: new_jwt.msg });
+
+	/* Get the user if from the token */
+	const { user_id } = get_jwt(new_jwt.token).payload;
+
+	/* Send the language update to the user container */
+	await fetch(`${process.env.USER_API_BASEURL_INTERNAL}/${user_id}`, {
+		method: 'PATCH',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({
+			language: language
+		})
+	})
+
+	/* Set the new token in the cookie */
+	return reply.setCookie('token', new_jwt.token, {
 			sameSite: 'None',
 			secure: true,
 			httpOnly: true,
 			path: '/',
 			expires: new Date(Date.now() + 24 * 60 * 60 * 1000)
-		}).code(200).send()
-		: reply.code(401).send({ error: new_jwt.msg });
+		}).code(200).send();
 }
