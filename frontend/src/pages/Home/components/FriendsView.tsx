e84c0d3302d190165d0e903a03be2096d182d00d
Miguel Guardia <mike.guardia0309@gmail.com>
2025-05-01 17:46:06 +0200

    Friends layout and search on navbar implemented using api (delete is not currently available)

--- frontend/src/pages/Home/components/FriendsView.tsx
+++ frontend/src/pages/Home/components/FriendsView.tsx
@@ -1,65 +1,48 @@
 import { useState } from 'react';
 import { FaCheck } from "react-icons/fa";
 import { ImCross } from "react-icons/im";
+
+// import { useAuth } from '@/hooks/useAuth';
 import { useLanguage } from '@/hooks/useLanguage';
 import { useNotification } from '@/hooks/useNotification';
-
-interface Friend {
-  id: number;
-  name: string;
-  photo: string;
-  online: boolean;
-}
-
-interface FriendRequest {
-  id: number;
-  name: string;
-  photo: string;
-}
+import { useFriends } from '@/hooks/useFriends';
+import { Friend } from '@/types/friendsContext';
 
 const FriendsView: React.FC = () => {
 
-  // useLanguage hook
+  /* useLanguage hook */
   const { t } = useLanguage();
+  
+  /* useAuth hook */
+  // const { user } = useAuth();
 
-  // useNotification hook
+  /* useNotification hook */
   const { addNotification } = useNotification();
 
-  // Estado para cambiar entre vista de amigos y solicitudes.
+  /* useState to set view */
   const [view, setView] = useState<'friends' | 'requests'>('friends');
 
-  // Datos de ejemplo para amigos.
-  const friends: Friend[] = [
-    { id: 1, name: 'Juan', photo: 'https://randomuser.me/api/portraits/men/8.jpg', online: true },
-    { id: 2, name: 'Ana', photo: 'https://randomuser.me/api/portraits/men/9.jpg', online: false },
-    { id: 3, name: 'Ana', photo: 'https://randomuser.me/api/portraits/men/9.jpg', online: false },
-    { id: 4, name: 'Ana', photo: 'https://randomuser.me/api/portraits/men/9.jpg', online: false },
-    { id: 5, name: 'Ana', photo: 'https://randomuser.me/api/portraits/men/9.jpg', online: false },
-    { id: 6, name: 'Ana', photo: 'https://randomuser.me/api/portraits/men/9.jpg', online: false },
-    { id: 7, name: 'Ana', photo: 'https://randomuser.me/api/portraits/men/9.jpg', online: false },
-    { id: 8, name: 'Ana', photo: 'https://randomuser.me/api/portraits/men/9.jpg', online: false },
-    { id: 9, name: 'Ana', photo: 'https://randomuser.me/api/portraits/men/9.jpg', online: false },
-    // Puedes agregar más amigos aquí
-  ];
+  /* useFriends hook */
+  const { friends, pending, acceptRequest, declineRequest }: { friends: Friend[], pending: Friend[], acceptRequest: (toUserId: number) => Promise<boolean>, declineRequest: (toUserId: number) => Promise<boolean> } = useFriends();
 
-  // Datos de ejemplo para solicitudes de amistad.
-  const friendRequests: FriendRequest[] = [
-    { id: 3, name: 'Carlos', photo: 'https://randomuser.me/api/portraits/men/10.jpg' },
-    // Puedes agregar más solicitudes aquí
-  ];
+  /* Function to handle accept a friend request */
+  const handleAcceptRequest = async (id: number) => {
+    
+    // send request to accept
+    await acceptRequest(id);
 
-  // Función para aceptar una solicitud.
-  const acceptRequest = (id: number) => {
-    console.log(`Solicitud de amistad aceptada: ${id}`);
-    // Aquí iría la lógica para aceptar la solicitud.
+    // add notification of success
     addNotification(`${t("notifications_friend_request_accepted")}`, 'success');
   };
+  
+  /* Function to handle decline a friend request */
+  const handleDeclineRequest = async (id: number) => {
 
-  // Función para rechazar una solicitud.
-  const rejectRequest = (id: number) => {
-    console.log(`Solicitud de amistad rechazada: ${id}`);
-    // Aquí iría la lógica para rechazar la solicitud.
-    addNotification(`${t("notifications_friend_request_rejected")}`, 'error');
+    // send request to accept
+    await declineRequest(id);
+
+    // add notification of success
+    addNotification(`${t("notifications_friend_request_rejected")}`, 'success');
   };
 
   return (
@@ -85,14 +68,14 @@ const FriendsView: React.FC = () => {
 
       {/* Vista de Amigos */}
       {view === 'friends' && (
-        <div className="grid grid-cols-1 gap-3 overflow-y-scroll scrollbar-thin h-full">
+        <div className="grid grid-cols-1 gap-3 overflow-y-scroll scrollbar-thin">
           {friends.map((friend) => (
             <div key={friend.id} className="flex items-center px-4 py-3 rounded-sm bg-background-hover border-2 border-background-secondary">
-              <img src={friend.photo} alt={friend.name} className="w-12 h-12 rounded-full border-2 border-text-tertiary mr-4" />
+              <img src={friend.avatar} alt={friend.alias} className="w-12 h-12 rounded-full border-2 border-text-tertiary mr-4 object-cover" />
               <div>
-                <p className="font-semibold">{friend.name}</p>
-                <p className={`text-sm ${friend.online ? 'text-green-500' : 'opacity-50 text-red-500'}`}>
-                  {friend.online ? `${ t("home_online") }` : `${ t("home_offline") }` }
+                <p className="font-semibold">{friend.alias}</p>
+                <p className={`text-sm ${friend.alias ? 'text-green-500' : 'opacity-50 text-red-500'}`}>
+                  {friend.alias ? `${ t("home_online") }` : `${ t("home_offline") }` }
                 </p>
               </div>
             </div>
@@ -103,20 +86,20 @@ const FriendsView: React.FC = () => {
       {/* Vista de Solicitudes de amistad */}
       {view === 'requests' && (
         <div className="grid grid-cols-1 gap-3 overflow-y-scroll scrollbar-thin">
-          {friendRequests.map((request) => (
-            <div key={request.id} className="flex items-center p-4 rounded-sm bg-background-hover border-2 border-background-secondary">
-              <img src={request.photo} alt={request.name} className="w-12 h-12 rounded-full border-2 border-text-tertiary mr-4" />
+          {pending.filter( (friend): friend is Friend => friend.from === friend.id ).map((request) => (
+            <div key={request.id} className="flex items-center px-4 py-3 rounded-sm bg-background-hover border-2 border-background-secondary">
+              <img src={request.avatar} alt={request.alias} className="w-12 h-12 rounded-full border-2 border-text-tertiary mr-4 object-cover" />
               <div className="flex-1">
-                <p className="font-semibold">{request.name}</p>
+                <p className="font-semibold">{request.alias}</p>
               </div>
               <div className='flex gap-2 md:gap-4'>
                 <FaCheck 
                   className="text-green-500 hover:cursor-pointer hover:text-green-300 transition-all duration-300" 
-                  onClick={() => acceptRequest(request.id)}
+                  onClick={() => handleAcceptRequest(request.id)}
                 />
                 <ImCross
                   className="text-red-500 hover:cursor-pointer hover:text-red-300 transition-all duration-300"
-                  onClick={() => rejectRequest(request.id)}
+                  onClick={() => handleDeclineRequest(request.id)}
                 />
               </div>
             </div>
