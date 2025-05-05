458b8b75c94980e270d8cc040490511ad22e40f2
Luiz-Pastor <luigpacr5@gmail.com>
2025-05-05 16:56:53 +0200

    Added the game tables (game and status)

--- backend-pong/src/database/schema.sql
+++ backend-pong/src/database/schema.sql
@@ -21,9 +21,35 @@ CREATE TABLE IF NOT EXISTS test (
   lose_count INTEGER NOT NULL DEFAULT 0,
   win_points INTEGER NOT NULL DEFAULT 0,
   lose_points INTEGER NOT NULL DEFAULT 0,
-  percentage REAL NOT NULL DEFAULT 0.0
+  percentage REAL NOT NULL DEFAULT 0.0,
 
   -- Constraints
   FOREIGN KEY (configuration) REFERENCES configuration(id) ON DELETE CASCADE
 );
 
+-- Game status
+CREATE TABLE IF NOT EXISTS game_status (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT UNIQUE
+);
+INSERT OR IGNORE INTO game_status(name) VALUES('Waiting');
+INSERT OR IGNORE INTO game_status(name) VALUES('In Progress');
+INSERT OR IGNORE INTO game_status(name) VALUES('Finished');
+
+-- Games
+CREATE TABLE IF NOT EXISTS games (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  status INTEGER NOT NULL,
+  date TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+  -- Players and puntuation
+  player_a INTEGER NOT NULL,
+  player_a_score INTEGER NOT NULL,
+  player_b INTEGER NOT NULL,
+  player_b_score INTEGER NOT NULL,
+
+  -- Constraints
+  FOREIGN KEY (status) REFERENCES game_status(id),
+  FOREIGN KEY (player_a) REFERENCES players(id) ON DELETE CASCADE,
+  FOREIGN KEY (player_b) REFERENCES players(id) ON DELETE CASCADE
+);
