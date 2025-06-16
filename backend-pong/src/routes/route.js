59273537080cf2732be70d60ed8e29448e68a4b4
Luiz-Pastor <luigpacr5@gmail.com>
2025-06-16 13:15:47 +0200

    Implemented the endpoint to change the players information

--- backend-pong/src/routes/route.js
+++ backend-pong/src/routes/route.js
@@ -1,5 +1,6 @@
 import player_create from "./player_create.js";
 import player_get from "./player_get.js";
+import player_modify from "./player_modify.js";
 
 export default async function (fastify, options)
 {
@@ -19,13 +20,33 @@ export default async function (fastify, options)
 	fastify.get('/player/:id', {
 		schema: {
 			params: {
-				type: 'object',
 				required: ['id'],
 				properties: {
 					id: { type: 'integer' }
 				}
 			}
 		}
-	}
-	, player_get);
+	}, player_get);
+
+	fastify.patch('/player/:id', {
+		schema: {
+			params: {
+				required: ['id'],
+				properties: {
+					id: { type: 'integer' }
+				}
+			},
+			body: {
+				type: 'object',
+				properties: {
+					configuration: { type: 'object' },
+					win_count: { type: 'integer', minimum: 1 },
+					lose_count: { type: 'integer', minimum: 1 },
+					win_points: { type: 'integer', minimum: 1 },
+					lose_points: { type: 'integer', minimum: 1 },
+				}
+			}
+
+		}
+	}, player_modify);
 }
