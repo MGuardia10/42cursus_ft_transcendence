1896c73b1ce50eb9aa6b0c491cee240b9dd50b49
Miguel Guardia <mike.guardia0309@gmail.com>
2025-06-20 21:02:37 +0200

    fixed conflict

--- backend-pong/src/routes/route.js
+++ backend-pong/src/routes/route.js
@@ -10,149 +10,182 @@ import ranking_specific_player from "./ranking_specific_player.js";
 
 /* This function is used to check if the cookie is present in the request */
 function cookieChecker(request, reply, done) {
-  if (!request.cookies || typeof request.cookies.token !== 'string')
+  if (!request.cookies || typeof request.cookies.token !== "string")
     return reply.status(400).send({ error: 'The "token" cookie is mandatory' });
   done();
 }
 
-export default async function (fastify, options)
-{
-	/*****************/
-	/* NOTE: Players */
-	/*****************/
+export default async function (fastify, options) {
+  /*****************/
+  /* NOTE: Players */
+  /*****************/
 
-	fastify.post('/players', {
-		schema: {
-			body: {
-				type: 'object',
-				required: ['user_id'],
-				properties: {
-					user_id: { type: 'integer', minimum: 1 }
-				}
-			},
-			additionalProperties: false
-		}
-	}, player_create);
+  fastify.post(
+    "/players",
+    {
+      schema: {
+        body: {
+          type: "object",
+          required: ["user_id"],
+          properties: {
+            user_id: { type: "integer", minimum: 1 },
+          },
+        },
+        additionalProperties: false,
+      },
+    },
+    player_create
+  );
 
-	fastify.get('/player/:id', {
-		schema: {
-			params: {
-				type: 'object',
-				required: ['id'],
-				properties: {
-					id: { type: 'integer' }
-				}
-			}
-		}
-	}, player_get);
+  fastify.get(
+    "/player/:id",
+    {
+      schema: {
+        params: {
+          type: "object",
+          required: ["id"],
+          properties: {
+            id: { type: "integer" },
+          },
+        },
+      },
+    },
+    player_get
+  );
 
-	fastify.patch('/player/:id', {
-		schema: {
-			params: {
-				type: 'object',
-				required: ['id'],
-				properties: {
-					id: { type: 'integer' }
-				}
-			},
-			body: {
-				type: 'object',
-				properties: {
-					configuration: { type: 'object' },
-					win_count: { type: 'integer', minimum: 1 },
-					lose_count: { type: 'integer', minimum: 1 },
-					win_points: { type: 'integer', minimum: 1 },
-					lose_points: { type: 'integer', minimum: 1 },
-				}
-			}
+  fastify.patch(
+    "/player/:id",
+    {
+      schema: {
+        params: {
+          type: "object",
+          required: ["id"],
+          properties: {
+            id: { type: "integer" },
+          },
+        },
+        body: {
+          type: "object",
+          properties: {
+            configuration: { type: "object" },
+            win_count: { type: "integer", minimum: 1 },
+            lose_count: { type: "integer", minimum: 1 },
+            win_points: { type: "integer", minimum: 1 },
+            lose_points: { type: "integer", minimum: 1 },
+          },
+        },
+      },
+    },
+    player_modify
+  );
 
-		}
-	}, player_modify);
+  fastify.delete(
+    "/player/:id",
+    {
+      schema: {
+        params: {
+          type: "object",
+          required: ["id"],
+          properties: {
+            id: { type: "integer" },
+          },
+        },
+      },
+      preValidation: cookieChecker,
+    },
+    player_delete
+  );
 
-	fastify.delete('/player/:id', {
-		schema: {
-			params: {
-				type: 'object',
-				required: ['id'],
-				properties: {
-					id: { type: 'integer' }
-				}
-			}
-		},
-		preValidation: cookieChecker
-	}, player_delete);
+  /*****************/
+  /* NOTE: Ranking */
+  /*****************/
 
-	/*****************/	
-	/* NOTE: Ranking */
-	/*****************/
+  fastify.get(
+    "/ranking",
+    {
+      schema: {
+        querystring: {
+          type: "object",
+          properties: {
+            limit: { type: "integer", minimum: 1, default: 10 },
+            page: { type: "integer", minimum: 1, default: 1 },
+            includeTop3: { type: "boolean", default: true },
+          },
+        },
+      },
+    },
+    ranking_all
+  );
 
-	fastify.get('/ranking', {
-		schema: {
-			querystring: {
-				type: 'object',
-				properties: {
-					limit: { type: 'integer', minimum: 1, default: 10 },
-					page: { type: 'integer', minimum: 1, default: 1 },
-					includeTop3: { type: 'boolean', default: true }
-				}
-			}
-		}
-	}, ranking_all);
+  fastify.get(
+    "/ranking/:id",
+    {
+      schema: {
+        params: {
+          type: "object",
+        },
+      },
+    },
+    ranking_specific_player
+  );
 
-	fastify.get('/ranking/:id', {
-		schema: {
-			params: {
-				type: 'object',
-      }
-		}
-	}, ranking_specific_player);
-      
-=======
-	/***************/
-	/* NOTE: Games */
-	/***************/
+  /***************/
+  /* NOTE: Games */
+  /***************/
 
-	fastify.post('/games', {
-		schema: {
-			body: {
-				type: 'object',
-				required: ['player_a_id', 'player_b_id'],
-				properties: {
-					player_a_id: { type: 'string' },
-					player_b_id: { type: 'string' },
-				}
-			}
-		}
-	}, game_create);
+  fastify.post(
+    "/games",
+    {
+      schema: {
+        body: {
+          type: "object",
+          required: ["player_a_id", "player_b_id"],
+          properties: {
+            player_a_id: { type: "string" },
+            player_b_id: { type: "string" },
+          },
+        },
+      },
+    },
+    game_create
+  );
 
-	fastify.get('/games', {
-		schema: {
-			querystring: {
-				type: 'object',
-				properties: {
-					player: { type: 'integer' }
-				}
-			},
-			additionalProperties: false
-		}
-	}, game_get);
+  fastify.get(
+    "/games",
+    {
+      schema: {
+        querystring: {
+          type: "object",
+          properties: {
+            player: { type: "integer" },
+          },
+        },
+        additionalProperties: false,
+      },
+    },
+    game_get
+  );
 
-	fastify.patch('/game/:id', {
-		schema: {
-			params: {
-				required: ['id'],
-				properties: {
-					id: { type: 'integer' }
-				}
-			},
-			body: {
-				type: 'object',
-				properties: {
-					player_a_score: { type: 'integer', minimum: 1 },
-					player_b_score: { type: 'integer', minimum: 1 },
-					state: { type: 'string' }
-				}
-			}
-		}
-	}, game_update);
+  fastify.patch(
+    "/game/:id",
+    {
+      schema: {
+        params: {
+          required: ["id"],
+          properties: {
+            id: { type: "integer" },
+          },
+        },
+        body: {
+          type: "object",
+          properties: {
+            player_a_score: { type: "integer", minimum: 1 },
+            player_b_score: { type: "integer", minimum: 1 },
+            state: { type: "string" },
+          },
+        },
+      },
+    },
+    game_update
+  );
 }
