4dcf94099e38ba66f4e5d8000a3ae323cd83d0fc
Salvador <salvazcon@gmail.com>
2025-06-24 07:25:22 -0400

    Implemented the endpoint to Join Tournament

--- backend-pong/src/routes/route.js
+++ backend-pong/src/routes/route.js
@@ -7,6 +7,7 @@ import player_get from "./player_get.js";
 import player_modify from "./player_modify.js";
 import ranking_all from "./ranking_all.js";
 import ranking_specific_player from "./ranking_specific_player.js";
+import tournament_join from './tournament_join.js';
 
 /* This function is used to check if the cookie is present in the request */
 function cookieChecker(request, reply, done) {
@@ -188,4 +189,21 @@ export default async function (fastify, options) {
     },
     game_update
   );
+
+  /********************/
+  /* NOTE: Tournament */
+  /********************/
+
+  fastify.post('/tournament/join', {
+    schema: {
+      body: {
+        type: 'object',
+        required: ['player_id', 'tournament_id'],
+        properties: {
+          player_id: { type: 'integer' },
+          tournament_id: { type: 'string' }
+        }
+      }
+    }
+  }, tournament_join);
 }
