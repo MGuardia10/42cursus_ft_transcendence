e7b298a330a92db93a98f1e26bcd4a29613dabc7
Salvador <salvazcon@gmail.com>
2025-06-30 11:19:27 -0400

    fixed Delete Tournament

--- backend-pong/src/routes/route.js
+++ backend-pong/src/routes/route.js
@@ -8,7 +8,6 @@ import player_modify from "./player_modify.js";
 import ranking_all from "./ranking_all.js";
 import ranking_specific_player from "./ranking_specific_player.js";
 import tournament_delete from './tournament_delete.js';
-import tournament_join from './tournament_join.js';
 
 /* This function is used to check if the cookie is present in the request */
 function cookieChecker(request, reply, done) {
--- backend-pong/src/routes/tournament_delete.js
+++ backend-pong/src/routes/tournament_delete.js
@@ -2,24 +2,27 @@ import db from "../database/database.js";
 
 export default async function tournament_delete(request, reply) {
     const { id } = request.params;
-
+    
     try {
-        const tournamentExists = db.prepare(`
-            SELECT id FROM tournaments WHERE id = ?
+        db.prepare("PRAGMA foreign_keys = ON").run();
+        
+        const games = db.prepare(`
+            SELECT game_id FROM tournament_games WHERE tournament_id = ?
+        `).all(id);
+        
+        db.prepare("DELETE FROM tournament_players WHERE tournament_id = ?").run(id);
+        db.prepare("DELETE FROM tournament_games WHERE tournament_id = ?").run(id);
+        db.prepare("DELETE FROM tournaments WHERE id = ?").run(id);
+        
+        const tournament = db.prepare(`
+            SELECT configuration_id FROM tournaments WHERE id = ?
         `).get(id);
-       
-        if (!tournamentExists)
-            return reply.code(404).send({ error: "Tournament not found" });
-
-        db.prepare(`
-            DELETE FROM configuration WHERE id = ?
-        `).run(tournament.configuration_id);
-
-        db.prepare(`
-            UPDATE tournaments SET active = ? WHERE id = ?
-        `).run(0, id);
+        
+        if (tournament)
+            db.prepare("DELETE FROM configuration WHERE id = ?").run(tournament.configuration_id);
 
         return reply.code(200).send();
+        
     } catch (err) {
         return reply.code(500).send({ error: err });
     }
