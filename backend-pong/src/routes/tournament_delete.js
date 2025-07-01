23240295edb522274d287d045be448accefbae46
Salvador <salvazcon@gmail.com>
2025-07-01 09:13:31 -0400

    Fixed Pragma use and Error control of DELETE

--- backend-pong/src/routes/tournament_delete.js
+++ backend-pong/src/routes/tournament_delete.js
@@ -4,26 +4,31 @@ export default async function tournament_delete(request, reply) {
     const { id } = request.params;
     
     try {
-        db.prepare("PRAGMA foreign_keys = ON").run();
-        
-        const games = db.prepare(`
-            SELECT game_id FROM tournament_games WHERE tournament_id = ?
-        `).all(id);
-        
-        db.prepare("DELETE FROM tournament_players WHERE tournament_id = ?").run(id);
-        db.prepare("DELETE FROM tournament_games WHERE tournament_id = ?").run(id);
-        db.prepare("DELETE FROM tournaments WHERE id = ?").run(id);
-        
         const tournament = db.prepare(`
-            SELECT configuration_id FROM tournaments WHERE id = ?
+            SELECT id FROM tournaments WHERE id = ?
         `).get(id);
         
-        if (tournament)
-            db.prepare("DELETE FROM configuration WHERE id = ?").run(tournament.configuration_id);
+        if (!tournament)
+            return reply.code(404).send({ error: 'Tournament not found' });
+
+        db.prepare("PRAGMA foreign_keys = OFF").run();
+        db.prepare("DELETE FROM tournament_players WHERE tournament_id = ?").run(id);
+        db.prepare("DELETE FROM tournament_games WHERE tournament_id = ?").run(id);
+        
+        const config_id = db.prepare(`
+            SELECT configuration_id FROM tournaments WHERE id = ?
+        `).get(id)?.configuration_id;
+        
+        if (config_id)
+            db.prepare("DELETE FROM configuration WHERE id = ?").run(config_id);
+
+        db.prepare("DELETE FROM tournaments WHERE id = ?").run(id);
+        db.prepare("PRAGMA foreign_keys = ON").run();
 
         return reply.code(200).send();
         
     } catch (err) {
+        db.prepare("PRAGMA foreign_keys = ON").run();
         return reply.code(500).send({ error: err });
     }
 }
